2ec0a8ccca9956a78150980bec3e8d99aa65e54b
shaneslone <slone.shane@gmail.com>
2021-12-09 17:49:22 -0500

    created interface for people type

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+interface IState {
+  people: {
+    name: string;
+    url: string;
+    age: number;
+    notes?: string;
+  }[];
+}
+
 function App() {
+  const [people, setPeople] = useState<IState['people']>([]);
+
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React With Typescript
-        </a>
-      </header>
+      <h1>People Invited To my Party</h1>
     </div>
   );
 }
